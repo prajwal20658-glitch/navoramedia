7be5b130942ef4451716431ef014582cf04b55b2
prajwal20658-glitch <prajwal20658@gmail.com>
2026-05-22 04:22:07 +0530

    Update index.js

--- navora-media1/workers-site/index.js
+++ navora-media1/workers-site/index.js
@@ -8,6 +8,15 @@ async function handleEvent(event) {
   try {
     return await getAssetFromKV(event);
   } catch (e) {
-    return new Response('Not found', { status: 404 });
+    try {
+      const url = new URL(event.request.url);
+      url.pathname = '/index.html';
+      const request = new Request(url.toString(), event.request);
+      return await getAssetFromKV(event, {
+        mapRequestToAsset: () => request,
+      });
+    } catch (e2) {
+      return new Response('Not found', { status: 404 });
+    }
   }
 }
